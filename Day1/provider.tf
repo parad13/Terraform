cfdcd31e7c9aa8c6cffa0d5dc2b52feb4fd86a41
Paras Waral <paraswaral2@gmail.com>
2023-05-13 23:44:05 +0530

    "Day1"

--- Day1/provider.tf
+++ Day1/provider.tf
@@ -1,4 +1,6 @@
-provider "aws" {
+# This file tells terraform which provider to use (like here aws, some other examples of provider are GCP, Azure, etc.).
+# You can see all available providers at https://registry.terraform.io/browse/providers
+provider "aws" { 
   region = "ap-south-1" # add a region
   aws_access_key_id = "string"
   aws_secret_access_key = "string"
